6e7e7678f7eea29e56da999e65ecdb11af1ddd23
sporter399 <sjporter399@gmail.com>
2019-07-08 11:27:06 -0600

    11:26 commit of user-signup

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ app.config['DEBUG'] = True
 def add_user_info():
     user_name = request.form['user-name']
     password = request.form['password']
+    rep_password = request.form['rep_password']
+    e_mail = request.form['e_mail']
     
 
     if (len(user_name) <= 3) or (user_name.strip() == ""):
         user_error = "User names must contain at least 4 characters. Please try again."
         return redirect("/?user_error=" + user_error)
 
-        user_name_escaped = cgi.escape(user_name, quote=True)
+    user_name_escaped = cgi.escape(user_name, quote=True)
 
     
 
@@ -26,8 +28,22 @@ def add_user_info():
 
     password_escaped = cgi.escape(password, quote=True)
 
+    if (rep_password != password):
+        rep_pass_error = "Your passwords do not match. Please try again."
+        return redirect("/?pass_error=" + rep_pass_error)
 
-    return render_template('add-confirmation.html', name=user_name, password=password)
+    password_escaped = cgi.escape(rep_password, quote=True)
+
+    if (len(e_mail) <= 3) or (len(e_mail) >= 20) or (e_mail.strip() == "") or ('@' not in e_mail) or ('.' not in e_mail):
+        mail_error = "Email addresses must contain '@' and '.' and be between 3 and 20 characters long. Please try again."
+        return redirect("/?mail_error=" + mail_error)
+
+    e_mail_escaped = cgi.escape(e_mail, quote=True)
+
+
+
+
+    return render_template('add-confirmation.html', name=user_name, password=password, rep_password=rep_password, e_mail=e_mail)
 
 
 
@@ -36,8 +52,12 @@ def add_user_info():
 def index():
     user_encoded_error = request.args.get("user_error")
     pass_encoded_error = request.args.get("pass_error")
+    rep_pass_encoded_error = request.args.get("rep_pass_error")
+    e_mail_encoded_error = request.args.get("mail_error")
+    
     return render_template('edit.html', user_error=user_encoded_error and cgi.escape(user_encoded_error, quote=True), 
-        pass_error=pass_encoded_error and cgi.escape(pass_encoded_error, quote=True))
+        pass_error=pass_encoded_error and cgi.escape(pass_encoded_error, quote=True), rep_pass_error=rep_pass_encoded_error and
+        cgi.escape(rep_pass_encoded_error, quote=True), mail_error=e_mail_encoded_error and cgi.escape(e_mail_encoded_error, quote=True))
 
 
 app.run()
